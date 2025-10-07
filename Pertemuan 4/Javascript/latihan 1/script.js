67462d8adb37817090c8f2bd9fa5b8d1c7b7596e
Eclipse-maq <aishahannah30@gmail.com>
2025-10-07 10:01:02 +0845

    Delete Pertemuan 4/Javascript directory

--- Pertemuan 4/Javascript/latihan 1/script.js
+++
@@ -1,9 +0,0 @@
-// ambil elemen
-const tombol = document.getElementById("btn");
-const teks = document.getElementById("teks");
-
-// event klik tonbol
-tombol.addEventListener("click", function () {
-    teks.textContent = "Teks berhasil diubah dengan js";
-    teks.style.color = "green";
-});
